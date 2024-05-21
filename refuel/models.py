a8e7efc04c0135c153b976f893fe076cc1db7251
TalhaAnik56 <aniktalha@gmail.com>
2024-05-21 17:55:53 +0600

    migration file

--- refuel/models.py
+++ refuel/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from segment.models import Branch
+
 # Create your models here.
 
 
@@ -16,6 +18,7 @@ RESTAURANT_STATUS = [(ACTIVE, "Active"), (OUT_OF_ORDER, "Out of order")]
 # Featured Image,Gallery will be added later
 class Restaurant(models.Model):
     name = models.CharField(max_length=50)
+    branch = models.ForeignKey(Branch, on_delete=models.PROTECT)
     status = models.CharField(max_length=1, choices=RESTAURANT_STATUS, default=ACTIVE)
     overview = models.TextField()
     cuisine = models.ManyToManyField(Cuisine)
